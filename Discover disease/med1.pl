b6be8383d8cb925e7a288931d784b8f865d87123
Mostafayasser01 <86493096+Mostafayasser01@users.noreply.github.com>
2021-06-25 21:34:41 +0200

    Add files via upload

---
+++ Discover disease/med1.pl
@@ -0,0 +1,16 @@
+
+	hypothesis(measles,fever,cough,conjunctivitis,runny_nose,rash).
+
+    	hypothesis(german_measles,fever,headache,runny_nose,rash).
+
+    	hypothesis(flu,fever,headache,body_ache,conjunctivitis,chills,sore_throat,runny_nose,cough).
+
+    	hypothesis(common_cold,headache,sneezing,sore_throat,runny_nose,chills).
+
+    	hypothesis(chicken_pox,fever,chills,body_ache,rash).
+
+    	hypothesis(measles,cough,sneezing,runny_nose).
+
+    	hypothesis(mumps,fever,swollen_glands).
+
+
